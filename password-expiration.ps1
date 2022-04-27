989589b33bda781740d684722403201f031ed73d
gabe31415 <scot.gabriel@gmail.com>
2022-04-26 20:49:44 -0500

    ..

--- password-expiration.ps1
+++
@@ -1,5 +0,0 @@
-Import-Module ActiveDirectory
-$MaxPwdAge = (Get-ADDefaultDomainPasswordPolicy).MaxPasswordAge.Days
-$expiredDate = (Get-Date).addDays(-$MaxPwdAge)
-$ExpiredUsers = Get-ADUser -Filter {(PasswordLastSet -gt $expiredDate) -and (PasswordNeverExpires -eq $false) -and (Enabled -eq $true)} -Properties PasswordNeverExpires, PasswordLastSet, Mail | Select-Object samaccountname, PasswordLastSet, @{name = "DaysUntilExpired"; Expression = {$_.PasswordLastSet - $ExpiredDate | Select-Object -ExpandProperty Days}} | Sort-Object PasswordLastSet
-$ExpiredUsers
